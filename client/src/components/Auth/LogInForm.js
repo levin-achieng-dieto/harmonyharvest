b0932559788ecdff30746387caece069618bd8f8
levin <levindieto8@gmail.com>
2024-02-16 16:19:26 +0300

    nothing really

--- client/src/components/Auth/LogInForm.js
+++ client/src/components/Auth/LogInForm.js
@@ -1,63 +1,64 @@
 import React, { useState } from "react";
+import "./LogInForm.css"
 
-function LogIn({onLogin}){
+function LogIn({ onLogin }) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const [errors, setErrors] = useState([])
     const [isLoading, setIsLoading] = useState(false)
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault()
         setIsLoading(true)
         fetch("/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password
-        }),
-    }).then((r) => {
-        setIsLoading(false);
-        if (r.ok){
-            r.json().then((user) => onLogin(user));
-        } else {
-            r.json().then((err) => setErrors(err.errors))
-        }
-    })
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            }),
+        }).then((r) => {
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => onLogin(user));
+            } else {
+                r.json().then((err) => setErrors(err.errors))
+            }
+        })
     }
 
-    return(
+    return (
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="username">Username</label>
                 <input
-                type="text"
-                id="username"
-                autoComplete="off"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                    type="text"
+                    id="username"
+                    autoComplete="off"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
                 />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
                 <input
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
             </div>
             <div>
                 <button variant="fill" color="primary" type="submit">
-                {isLoading ? "Loading..." : "Login"}
+                    {isLoading ? "Loading..." : "Login"}
                 </button>
             </div>
             <div>
                 {errors.map((err) => (
-                <error key={err}>{err}</error>
+                    <error key={err}>{err}</error>
                 ))}
             </div>
         </form>
